d436c0c66ca701099a3526868431d23cac52fc9e
Pedro Goulart <ph.goulart.almeida@gmail.com>
2021-11-18 19:45:48 -0300

    book some e every

--- Bloco 8 - Higher Order Functions do JavaScript ES6/forEach.js
+++ Bloco 8 - Higher Order Functions do JavaScript ES6/forEach.js
@@ -109,10 +109,42 @@ console.log(getNamedBook()); */
 
 // 4 - Ordene os livros por data de lançamento em ordem decrescente.
 
-function booksOrderedByReleaseYearDesc() {
+/* function booksOrderedByReleaseYearDesc() {
     return books.sort((value1, value2) => value2.releaseYear - value1.releaseYear)
 }
 console.log(booksOrderedByReleaseYearDesc());
+ */
+// 5 - Faça uma função que retorne true , se todas as pessoas autoras nasceram no século XX, ou false , caso contrário.
+
+
+/* function everyoneWasBornOnSecXX() {
+    return books.every((value) => value.author.birthYear > 1900 && value.author.birthYear < 1800)
+}
+console.log(everyoneWasBornOnSecXX()); */
+
+
+// 6 - Faça uma função que retorne true , se algum livro foi lançado na década de 80, e false , caso contrário.
+
+
+
+/* function someBookWasReleaseOnThe80s() {
+    return books.some((value) => value.releaseYear >= 1980 && value.releaseYear < 1990)
+}
+console.log(someBookWasReleaseOnThe80s()); */
+
+
+// 7 - Faça uma função que retorne true , caso nenhum author tenha nascido no mesmo ano, e false , caso contrário.
+
+
+
+
+function authorUnique() {
+    return books.every((book) =>
+        !books.some((bookSome) =>
+            (bookSome.author.birthYear === book.author.birthYear)
+            && (bookSome.author.name !== book.author.name)));
+}
+console.log(authorUnique());
 
 
 
