75a836ee2268233e460f75ff55565777c1d95bd3
Pedro Goulart <ph.goulart.almeida@gmail.com>
2021-11-19 13:47:33 -0300

    refatorando

---
+++ Bloco 8 - Higher Order Functions do JavaScript ES6/filter.js
@@ -0,0 +1,22 @@
+const arrayMyStudents = ['Maria', 'Manuela', 'Jorge', 'Ricardo', 'Wilson'];
+
+const removeName = (name, students) =>
+    students.filter((value) => value !== name)
+
+const remover = removeName('Ricardo', arrayMyStudents);
+console.log(remover);
+
+
+const objPeople = [
+    { name: 'José', age: 21 },
+    { name: 'Lucas', age: 19 },
+    { name: 'Maria', age: 16 },
+    { name: 'Gilberto', age: 18 },
+    { name: 'Vitor', age: 15 },
+];
+
+const age2 = (age) => (
+    age.filter((value) => (value.age <= 18))
+);
+console.log(age2(objPeople));
+  // [ { name: 'Maria', age: 16 }, { name: 'Vitor', age: 15 } ]
