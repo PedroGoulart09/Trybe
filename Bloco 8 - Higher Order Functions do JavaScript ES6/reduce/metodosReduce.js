2dea72136dd84315ecaae6b16685caf554ef88e1
Pedro Goulart <ph.goulart.almeida@gmail.com>
2021-11-22 18:17:51 -0300

    refatorando

--- Bloco 8 - Higher Order Functions do JavaScript ES6/reduce/metodosReduce.js
+++ Bloco 8 - Higher Order Functions do JavaScript ES6/reduce/metodosReduce.js
@@ -5,7 +5,13 @@ const { cervejas, clientes } = require('./dados');
 // Verificar quantos clientes são maior de idade e quantos são menor de idade
 // usando reduce
 
-const clientesPorIdade = { adulto: 0, menorIdade: 0 };
+const clientesPorIdade = clientes.reduce(
+    (accIdade, cliente) => {
+        if (cliente.idade < 18) return { adulto: accIdade.adulto, menorIdade: accIdade.menorIdade + 1 };
+        return { adulto: accIdade.adulto + 1, menorIdade: accIdade.menorIdade };
+    },
+    { adulto: 0, menorIdade: 0 }
+);
 
 console.log(clientesPorIdade);
 
@@ -13,7 +19,17 @@ console.log(clientesPorIdade);
 // para cada cerveja:
 // valorEmEstoqueDeCervejas: [{codigo:176, descricao:Pilsner Urquell 500ML, valor_em_estoque: R$ 4.000,00}, {...}]
 
-const valorEmEstoqueDeCervejas = [];
+const valorEmEstoqueDeCervejas = cervejas.reduce((acc, cerveja) => {
+    acc.push({
+        codigo: cerveja.codigo,
+        descricao: cerveja.descricao,
+        valor_em_estoque: (cerveja.valor * cerveja.estoque).toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+        }),
+    });
+    return acc;
+}, []);
 
 console.log(valorEmEstoqueDeCervejas);
 
