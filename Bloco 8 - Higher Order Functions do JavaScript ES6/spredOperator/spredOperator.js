3daf0bbf954c81f60cf51f0a99589b8469abdd66
Pedro Goulart <ph.goulart.almeida@gmail.com>
2021-11-23 20:17:05 -0300

    refatorando

--- Bloco 8 - Higher Order Functions do JavaScript ES6/spredOperator/spredOperator.js
+++ Bloco 8 - Higher Order Functions do JavaScript ES6/spredOperator/spredOperator.js
@@ -32,11 +32,6 @@ const fruitSalad = (fruit, additional) => {
 console.log(fruitSalad(specialFruit, additionalItens));
 
 
-const teste = (...args) => args.reduce((acc, val) => acc + val, 0);
-console.log(teste(1, 2, 3));
-
-const sum = (...args) => args.reduce((accumulator, current) => accumulator + current, 0);
-console.log(sum(4, 7, 8, 9, 60));
 
 
 
