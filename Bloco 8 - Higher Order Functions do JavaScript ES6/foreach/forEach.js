137ea8ff0a4f88d48df70b4a4c042a28ebe21371
Pedro Goulart <ph.goulart.almeida@gmail.com>
2021-11-24 08:58:54 -0300

    refatorando

--- Bloco 8 - Higher Order Functions do JavaScript ES6/foreach/forEach.js
+++ Bloco 8 - Higher Order Functions do JavaScript ES6/foreach/forEach.js
@@ -1,81 +1,4 @@
-const books = [
-    {
-        id: 1,
-        name: 'As Crônicas de Gelo e Fogo',
-        genre: 'Fantasia',
-        author: {
-            name: 'George R. R. Martin',
-            birthYear: 1948,
-        },
-        releaseYear: 1991,
-    },
-    {
-        id: 2,
-        name: 'O Senhor dos Anéis',
-        genre: 'Fantasia',
-        author: {
-            name: 'J. R. R. Tolkien',
-            birthYear: 1892,
-        },
-        releaseYear: 1954,
-    },
-    {
-        id: 3,
-        name: 'Fundação',
-        genre: 'Ficção Científica',
-        author: {
-            name: 'Isaac Asimov',
-            birthYear: 1920,
-        },
-        releaseYear: 1951,
-    },
-    {
-        id: 4,
-        name: 'Duna',
-        genre: 'Ficção Científica',
-        author: {
-            name: 'Frank Herbert',
-            birthYear: 1920,
-        },
-        releaseYear: 1965,
-    },
-    {
-        id: 5,
-        name: 'A Coisa',
-        genre: 'Terror',
-        author: {
-            name: 'Stephen King',
-            birthYear: 1947,
-        },
-        releaseYear: 1986,
-    },
-    {
-        id: 6,
-        name: 'O Chamado de Cthulhu',
-        genre: 'Terror',
-        author: {
-            name: 'H. P. Lovecraft',
-            birthYear: 1890,
-        },
-        releaseYear: 1928,
-    },
-];
 
-function authorBornIn1947() {
-    return books.find((value) => value.author.birthYear === 1947).author.name;
-};
-console.log(authorBornIn1947());
-
-const names = ['João', 'Irene', 'Fernando', 'Maria'];
-
-const findNameWithFiveLetters = () => {
-    let arr = ''
-    names.find((value) => value.length > arr)
-    return arr;
-
-}
-
-console.log(findNameWithFiveLetters());
 
 
 // 2 - Retorne o nome do livro de menor nome.
@@ -88,13 +11,6 @@ function smallerName() {
 }
 console.log(smallerName());
 
-// 3 - Encontre o primeiro livro cujo nome possui 26 caracteres.
-
-
-function getNamedBook() {
-    return books.find((value) => value.name.length === 26);
-}
-console.log(getNamedBook());
 
 
 // 4 - Ordene os livros por data de lançamento em ordem decrescente.
@@ -104,35 +20,8 @@ function booksOrderedByReleaseYearDesc() {
 }
 console.log(booksOrderedByReleaseYearDesc());
 
-// 5 - Faça uma função que retorne true , se todas as pessoas autoras nasceram no século XX, ou false , caso contrário.
 
 
-function everyoneWasBornOnSecXX() {
-    return books.every((value) => value.author.birthYear > 1900 && value.author.birthYear < 1800)
-}
-console.log(everyoneWasBornOnSecXX());
-
-
-// 6 - Faça uma função que retorne true , se algum livro foi lançado na década de 80, e false , caso contrário.
-
-
-
-function someBookWasReleaseOnThe80s() {
-    return books.some((value) => value.releaseYear >= 1980 && value.releaseYear < 1990)
-}
-console.log(someBookWasReleaseOnThe80s());
-
-
-// 7 - Faça uma função que retorne true , caso nenhum author tenha nascido no mesmo ano, e false , caso contrário.
-
-
-function authorUnique() {
-    return books.every((book) =>
-        !books.some((bookSome) =>
-            (bookSome.author.birthYear === book.author.birthYear)
-            && (bookSome.author.name !== book.author.name)));
-}
-console.log(authorUnique());
 
 const emailListInData = [
     'roberta@email.com',
