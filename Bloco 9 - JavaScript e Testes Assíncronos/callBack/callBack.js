4c657953c1d39c0ebf216c3cb64321cfb29a9eb6
Pedro Goulart <ph.goulart.almeida@gmail.com>
2021-11-29 14:26:29 -0300

    aula

--- Bloco 9 - JavaScript e Testes Assíncronos/callBack/callBack.js
+++ Bloco 9 - JavaScript e Testes Assíncronos/callBack/callBack.js
@@ -65,7 +65,7 @@ const getUser = (callback) => {
         lastName: "Ivanovich",
         nationality: "Russian"
     };
-    return callback(userToReturn)
+    return callback(userToReturn);
 };
 
 console.log(getUser(userFullName)); // complete a chamada da função getUser de modo que o retorno seja: "Hello! My name is Ivan Ivanovich"
@@ -91,11 +91,47 @@ const getUser = (callback) => {
             lastName: "Ivanovich",
             nationality: "Russian",
         };
-        console.log(callback(user));
 
+        console.log(callback(user));
     }, delay());
 };
 
 getUser(userFullName); // deve imprimir "Hello! My name is Ivan Ivanovich" depois de um certo tempo
 getUser(userNationality); // deve imprimir "Ivan is Russian" depois de um certo tempo
 
+
+
+
+// 1 - A função getCountry abaixo tem aproximadamente 50% de chance de obter com sucesso um país, tendo um callback para poder ser feita qualquer operação sobre o país retornado. Adicione um outro callback para getCountry , de forma que trate a mensagem de erro retornada.
+
+const countryName = ({ name }) => console.log(`Returned country is ${name}`);
+const countryCurrency = ({ name, currency }) => console.log(`${name}'s currency is the ${currency}`);
+
+const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+
+const printErrorMessage = (error) => console.log(`Error getting country: ${error}`);
+
+const getCountry = (onSuccess, callback) => {
+    setTimeout(() => {
+        const didOperationSucceed = Math.random() >= 0.5;
+        if (didOperationSucceed) {
+            const country = {
+                name: "Brazil",
+                hdi: 0.759,
+                currency: "Real",
+            };
+            onSuccess(country);
+        }
+        else {
+            const errorMessage = "Country could not be found";
+            callback(errorMessage);
+
+        }
+    }, delay());
+};
+
+// Deve imprimir "Returned country is Brazil" no sucesso, ou "Error getting country: Country could not be found" em falha
+getCountry(countryName, printErrorMessage);
+
+// Deve imprimir "Brazil's currency is the Real" no sucesso, ou "Error getting country: Country could not be found" em falha
+getCountry(countryCurrency, printErrorMessage);
