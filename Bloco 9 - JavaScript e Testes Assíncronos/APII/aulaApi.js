d60e2f80a43929820b75ae2c4b06b57faa3a5586
Pedro Goulart <ph.goulart.almeida@gmail.com>
2021-11-30 20:22:10 -0300

    aula

--- Bloco 9 - JavaScript e Testes Assíncronos/APII/aulaApi.js
+++ Bloco 9 - JavaScript e Testes Assíncronos/APII/aulaApi.js
@@ -1,4 +1,4 @@
-// apiScript.js     
+const h2 = document.getElementById('jokeContainer');
 const API_URL = 'https://icanhazdadjoke.com/';
 
 const fetchJoke = () => {
@@ -6,10 +6,9 @@ const fetchJoke = () => {
         method: 'GET',
         headers: { 'Accept': 'application/json' }
     };
-
     fetch(API_URL, myObject)
         .then(response => response.json())
-        .then(data => console.log(data));
+        .then(data => h2.innerHTML = data.joke);
 };
 
 window.onload = () => fetchJoke();
