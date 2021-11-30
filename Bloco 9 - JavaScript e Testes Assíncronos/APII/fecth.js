ad4ebc233f1efdca94c067df480391fb2d1701bc
Pedro Goulart <ph.goulart.almeida@gmail.com>
2021-11-30 14:44:43 -0300

    aula

--- Bloco 9 - JavaScript e Testes Assíncronos/APII/fecth.js
+++ Bloco 9 - JavaScript e Testes Assíncronos/APII/fecth.js
@@ -36,3 +36,43 @@ fetchJoke();
 
 // Algo deu errado :( 
 // TypeError: Only absolute URLs are supported
+
+
+
+//REFATORANDO A FUNÇÃO PARA O USO DO async
+
+const fetch = require('node-fetch');
+
+const fetchJoke = async () => {
+    const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+    const result = await fetch(url)
+        .then((response) => response.json())
+        .then((data) => console.log(data.value))
+        .catch((error) => console.log(`Algo deu errado :( \n${error}`));
+
+    return result;
+}
+
+fetchJoke();
+
+// Chuck Norris can write multi-threaded applications with a single thread.
+
+
+const fetch = require('node-fetch');
+
+const fetchJoke = async () => {
+    const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data.value);
+    } catch (error) {
+        console.log(`Algo deu errado :( \n${error}`);
+    }
+}
+
+fetchJoke();
+
+// Chuck Norris can write multi-threaded applications with a single thread.
