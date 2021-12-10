76888dc94b4ea83736e845d0f9be0104463d509f
Pedro Goulart <ph.goulart.almeida@gmail.com>
2021-12-10 16:30:20 -0300

    bloco 10 React

---
+++ Bloco 10 - Introdução à React/Aula-PropTypes/my-pokedex/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import './App.css';
+import ReactData from './data'
+import Componente from './Component'
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        < ReactData />
+        < Componente />
+      </div>
+    )
+  }
+}
+
+
+export default App;
---
+++ Bloco 10 - Introdução à React/Aula-PropTypes/my-pokedex/src/Component.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Componente extends Component {
+    render() {
+        return (
+            <div>
+                oi eai
+            </div>
+        )
+    }
+}
+export default Componente
