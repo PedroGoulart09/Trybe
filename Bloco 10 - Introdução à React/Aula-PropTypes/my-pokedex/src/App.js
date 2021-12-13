a90c080047e615a7692ea53b7a5b323710a93deb
Pedro Goulart <ph.goulart.almeida@gmail.com>
2021-12-13 19:58:51 -0300

    bloco 10 React

--- Bloco 10 - Introdução à React/Aula-PropTypes/my-pokedex/src/App.js
+++ Bloco 10 - Introdução à React/Aula-PropTypes/my-pokedex/src/App.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react'
-import './App.css';
-import ReactData from './data'
-import Componente from './Component'
-
+import Pokemon from './data'
 
 
 class App extends Component {
   render() {
     return (
       <div>
-        < ReactData />
-        < Componente />
+        {Pokemon.map((pokemons) => (
+          <section>
+            <ul>
+              <li>{pokemons.name}</li>
+              <li>{pokemons.type}</li>
+              <li>{`average Weight : ${pokemons.averageWeight.value} ${pokemons.averageWeight.measurementUnit}`}</li>
+
+              <img src={pokemons.image} alt="pokemons" />
+
+            </ul>
+
+          </section>
+        ))}
+
       </div>
     )
   }
