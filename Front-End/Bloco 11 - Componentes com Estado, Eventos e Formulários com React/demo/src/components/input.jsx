7da70025abcaededb0beeffc2ee20b55c36ee27f
Pedro Goulart <ph.goulart.almeida@gmail.com>
2022-01-06 15:36:02 -0300

    finalizado

--- Front-End/Bloco 11 - Componentes com Estado, Eventos e Formulários com React/demo/src/components/input.jsx
+++ Front-End/Bloco 11 - Componentes com Estado, Eventos e Formulários com React/demo/src/components/input.jsx
@@ -1,43 +1,47 @@
 import React, { Component } from 'react'
 
 export default class input extends Component {
-   constructor(props) {
-       super(props)
-       this.state = {
-           valueInput: '',
-           textarea : '',
-           checked : false
-       }
-       this.input = this.input.bind(this)
-   }
+    constructor(props) {
+        super(props)
+        this.state = {
+            valueInput: '',
+            textarea: '',
+            checked: false
+        }
+        this.input = this.input.bind(this)
+    }
 
-//FORMA DE UMA FUNÇÃO PARA LIDAR COM TODOS OS ELEMENTOS JUNTOS.
+    //FORMA DE UMA FUNÇÃO PARA LIDAR COM TODOS OS ELEMENTOS JUNTOS.
 
-   input(e){
-       const {name} = e.target
-       const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value
-       this.setState({
-           [name]: value
-       })
-       
-   }
+    input(e) {
+        
+        const { name } = e.target
+        const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value
+        this.setState({
+            [name]: value
+        })
+
+    }
 
 
     render() {
         return (
-            <div>
-                <input name="valueInput" value={this.state.valueInput} onChange={this.input}></input>
-                <p>{this.state.valueInput}</p>
-
+            <form>
                 <div>
-                    <label>
-                Qual seu Estado Favorito:
-                    <textarea name="textarea" value={this.state.textarea} onChange={this.input}></textarea>
-                    </label>
+                    <input name="valueInput" value={this.state.valueInput} onChange={this.input}></input>
+                    <p>{this.state.valueInput}</p>
 
-                    <input type="checkbox" name="checked" value={this.state.checked} onChange={this.input}></input>
+                    <div>
+                        <label>
+                            Qual seu Estado Favorito:
+                            <textarea name="textarea" value={this.state.textarea} onChange={this.input}></textarea>
+                        </label>
+
+                        <input type="checkbox" name="checked" value={this.state.checked} onChange={this.input}></input>
+                    </div>
                 </div>
-            </div>
+            </form>
+
         )
     }
 }
