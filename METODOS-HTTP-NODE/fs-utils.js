0ad3642984a655789d325335f12876663c4c1dfe
Pedro Goulart <ph.goulart.almeida@gmail.com>
2022-04-19 20:55:56 -0300

    requisitos

---
+++ METODOS-HTTP-NODE/fs-utils.js
@@ -0,0 +1,12 @@
+const fs = require('fs/promises');
+
+async function getSimpsons() {
+    const fileContent = await fs.readFile('./simpsons.json', 'utf-8');
+    return JSON.parse(fileContent);
+}
+
+function setSimpsons(newSimpsons) {
+    return fs.writeFile('./simpsons.json', JSON.stringify(newSimpsons));
+}
+
+module.exports = { getSimpsons, setSimpsons };
